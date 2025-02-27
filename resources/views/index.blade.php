d2193d45c06b64341d0e84fc6a1a526547df34e2
Farhan0511 <programingsukses.com>
2025-02-27 21:36:11 +0700

    displays the user table

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,34 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-   
-    <div>
-        <table>
-            <tr>
-                <th>Nama</th>
-                <th>Email</th>
-                <th>Aksi</th>
-            </tr>
-            
-            @foreach ($data as $d)
-            <tr>
-                <td>{{ $d->name }}</td>
-                <td>{{ $d->email }}</td>
-                <td></td>
-            </tr>
-            @endforeach
-        </table>
-    </div>
+@section('content')
+    <main class="app-main">
+        <!--begin::App Content Header-->
+        <div class=" app-content-header">
+            <!--begin::Container-->
+            <div class="container-fluid">
+                <!--begin::Row-->
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h3 class="mb-0">User</h3>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-end">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Data User</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="container mt-5">
+                <table class="table table-bordered text-center shadow-sm">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Nama </th>
+                            <th>Alamat</th>
+                            <th>Pekerjaan</th>
+                            <th>Usia</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Andi</td>
+                            <td>Jakarta Selatan</td>
+                            <td>Web Designer</td>
+                            <td>21</td>
+                        </tr>
+                        <tr>
+                            <td>Budi</td>
+                            <td>Bandung</td>
+                            <td>Web Developer</td>
+                            <td>26</td>
+                        </tr>
+                        <tr>
+                            <td>Cahyo</td>
+                            <td>Bekasi</td>
+                            <td>Web Programmer</td>
+                            <td>29</td>
+                        </tr>
+                        <tr>
+                            <td>Darma</td>
+                            <td>Bali</td>
+                            <td>Teknisi</td>
+                            <td>35</td>
+                        </tr>
+                    </tbody>
+                </table>
     
-
-</body>
-
-</html>
+            </div>
+        </div>
+    </main>
+@endsection
